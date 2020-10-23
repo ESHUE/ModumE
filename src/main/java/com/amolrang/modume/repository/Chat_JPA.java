c162008b8c5ec96448939bd131c317bfcc549c2c
Hazel-K <hyojin4588@naver.com>
2020-10-23 16:14:45 +0900

    JPA 기능 체크 완료

---
+++ src/main/java/com/amolrang/modume/repository/Chat_JPA.java
@@ -0,0 +1,16 @@
+package com.amolrang.modume.repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Chat_JPA {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+}
