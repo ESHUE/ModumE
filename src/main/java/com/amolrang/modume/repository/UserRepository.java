7fd5b2e4af287183dfde6ebb00ff34836ef31596
Hazel-K <hyojin4588@naver.com>
2020-10-26 16:28:30 +0900

    Repository 수정

--- src/main/java/com/amolrang/modume/repository/UserRepository.java
+++ src/main/java/com/amolrang/modume/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.amolrang.modume.model.User_JPA;
 
 public interface UserRepository extends JpaRepository<User_JPA, Integer>{
-	String findByUsername(String username);
+	User_JPA findByUsername(String username);
 }
