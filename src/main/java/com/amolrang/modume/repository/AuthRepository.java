45c08473a4dc62d40beafb791c9320dae679df73
류욱준 <corn1107@hanmail.net>
2020-10-26 16:28:30 +0900

    수정중

--- src/main/java/com/amolrang/modume/repository/AuthRepository.java
+++ src/main/java/com/amolrang/modume/repository/AuthRepository.java
@@ -1,9 +1,9 @@
-
 package com.amolrang.modume.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.amolrang.modume.model.Authorize_JPA;
 
-public interface AuthRepository extends JpaRepository<Authorize_JPA, Integer>{}
+public interface AuthRepository extends JpaRepository<Authorize_JPA, Integer>{
+}
 
