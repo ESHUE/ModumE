53a3edd5337aecc5d70a1958286ce6d0be6d977d
Dendrocopos-kr <lunatic787973@gmail.com>
2020-10-26 17:50:24 +0900

    코드 정리1

--- src/main/java/com/amolrang/modume/config/SecurityConfig.java
+++ src/main/java/com/amolrang/modume/config/SecurityConfig.java
@@ -35,11 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+		String[] ignoringUrls = {"/css/**","/js/**","/img/**"};
 		// static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
 //		web.ignoring().antMatchers("/favicon.ico");
-		web.ignoring().antMatchers("/css/**");
-		web.ignoring().antMatchers("/js/**");
-		web.ignoring().antMatchers("/img/**");
+		web.ignoring().antMatchers(ignoringUrls);
 	}
 
 	@Override
@@ -52,6 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		String[] permitAllurl = {"/","/main","/join","/join","/login","/login/oauth2/**"};
+		
 		http.csrf().disable(); // 활성화되어 있으면 위변조 사이트인지 판별해줌, 나중에 활성화시켜야 함
 		//로그아웃추가
 		http.logout().invalidateHttpSession(true).deleteCookies("JSESSIONID").logoutSuccessUrl("/");
@@ -60,25 +61,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// antMatchers("주소") -> 주소 매칭
 		// /admin 주소는 ADMIN 권한을 가진 사람만 접속 가능
 		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN"); 
-		
 		// 권한 필요없는 경로 추가
 		// loginPage("/login") -> 기본 spring security 로그인 페이지를 내 /login 주소로 매핑함
 		// permitAll은 모든 권한 다 됨
 		// 로그인 form에서 아이디는 name=username인 input을 기본으로 인식하는데, usernameParameter() 메서드를 통해 파라미터명을 변경할 수 있음
 		http.formLogin().loginPage("/login").usernameParameter("user_id").defaultSuccessUrl("/loginAction").permitAll();
-		http.authorizeRequests().antMatchers("/main").permitAll();
-		http.authorizeRequests().antMatchers("/join").permitAll();
-		http.authorizeRequests().antMatchers("/").permitAll();
-		http.authorizeRequests().antMatchers("/login").permitAll();
-		
-		http.authorizeRequests().antMatchers("/login/oauth2/**").permitAll()
+		http.authorizeRequests().antMatchers(permitAllurl).permitAll()
 		.and()
 		.oauth2Login().loginPage("/login").defaultSuccessUrl("/login_success")
 		.clientRegistrationRepository(clientRegistrationRepository())
-		.authorizedClientService(authorizedClientService());
-		
+		.authorizedClientService(authorizedClientService())
 		// 권한없이 접근한 페이지로 보내는 곳
-		http.exceptionHandling().accessDeniedPage("/denied");
+		.and().exceptionHandling().accessDeniedPage("/denied");
 	}
 	
 	// 패스워드 암호화
