5901f40ee6e06b6fdab7f94d03b12e726dd2f041
Hazel-K <hyojin4588@naver.com>
2020-10-28 11:02:29 +0900

    JPA 옵션 수정

--- src/main/java/com/amolrang/modume/repository/UserBoardRepository.java
+++ src/main/java/com/amolrang/modume/repository/UserBoardRepository.java
@@ -9,6 +9,6 @@ import com.amolrang.modume.model.Userboard_JPA;
 
 
 public interface UserBoardRepository extends JpaRepository<Userboard_JPA, Integer>{
-	@Query(value = "SELECT A.USERBOARD_SEQ, A.title, A.content, A.hits, DATE_FORMAT(A.r_date, '%m-%d') AS r_date, B.username, B.profileImg FROM Userboard_JPA A JOIN User_JPA B ON A.MAIN_SEQ = B.MAIN_SEQ")
+	@Query(value = "SELECT A.USERBOARD_SEQ, A.title, A.content, A.hits, DATE_FORMAT(A.r_date, '%m-%d') AS r_date, B.username, B.profileImg FROM Userboard_JPA A JOIN User_JPA B ON A.user = B.MAIN_SEQ")
 	List<Userboard_JPA> findBoardWithMAIN_SEQ();
 }
