3fa612257b63edd9335b2afcbb5f57ecb450c320
Hazel-K <hyojin4588@naver.com>
2020-11-02 12:48:00 +0900

    게시글 image insert 쿼리 구현

--- src/main/java/com/amolrang/modume/model/BoardimgId_JPA.java
+++ src/main/java/com/amolrang/modume/model/BoardimgId_JPA.java
@@ -6,6 +6,14 @@ import lombok.Data;
 
 @Data
 public class BoardimgId_JPA implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private int imgseq;
 	private Userboard_JPA boardseq;
+	
+	public BoardimgId_JPA() {}
+	
+	public BoardimgId_JPA(int imgseq, Userboard_JPA boardseq) {
+		this.imgseq = imgseq;
+		this.boardseq = boardseq;
+	}
 }
