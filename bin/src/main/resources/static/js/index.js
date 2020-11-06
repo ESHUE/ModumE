d0fd873fa3a828c2f467177c803ab243bd082817
류욱준 <corn1107@hanmail.net>
2020-11-06 15:21:30 +0900

    알수없는 파일 엉키는 오류 수정

--- bin/src/main/resources/static/js/index.js
+++ bin/src/main/resources/static/js/index.js
@@ -2,21 +2,51 @@ var sectionContainer = document.querySelector('.sectionContainer');
 var centralContainer = document.querySelector('.centralContainer');
 var centralMenu1_2 = document.querySelector('.centralMenu1_2');
 
-var stompClient = null;
-var inputChat = null;
-var username = null;
-var inputUl = null;
+//채팅을 위한 전역변수들( 미리 초기화 )
+var roomId = null;
+var member = null;
+var message = null;
 
-
-
-function chatInit() {
+function findVideo(evt) {
+   evt.preventDefault();
+   const keyword = document.querySelector('#searchVideo').value;
+   // console.log(keyword);
+	axios.get('/googleSearch', {
+      params : {
+         keyword
+      }
+   }).then(function(res) {
+      // console.log(res);
+      showSearchList(res);
+   }).then(function() {
+      var searchSwiper = new Swiper('.searchResultContainer>.swiper-container', {
+         slidesPerView: 4,
+         spaceBetween: 10,
+         slidesPerGroup: 4,
+         loop: false,
+         loopFillGroupWithBlank: true,
+         mousewheel: true,
+         pagination: {
+           el: '.swiper-pagination',
+           clickable: true,
+         },
+         navigation: {
+           nextEl: '.swiper-button-next',
+           prevEl: '.swiper-button-prev',
+         },
+      });
+   })
+}
+function chatInit(temp) {
+   member = temp;
+   console.log('값 체크:' + member)
     const chatContainer = document.querySelector('.chatContainer');
     const boardContainer = document.querySelector('.boardContainer');
     if(boardContainer != null) {
         closeContainer(boardContainer);
     }
     if(chatContainer == null) {
-        openChat();
+      openChat(temp)
     } else {
         closeContainer(chatContainer);
     }
@@ -35,27 +65,112 @@ function boardInit() {
     }
 }
 
-function openChat() {
+function openChat(member) {
     const makeDiv = document.createElement('div');
     makeDiv.className = 'chatContainer';
+	 makeDiv.setAttribute('id','chatContainer')
     sectionContainer.append(makeDiv);
+    const chatList = document.createElement('div');
+    chatList.className = 'chatList';
+    makeDiv.append(chatList)
+    TestDetail(roomId,member)
    /*채팅관련 창들 */
-   const chatDiv = document.createElement('div');
-   chatDiv.className = 'hidden';``
-   makeDiv.append(chatDiv);
-   inputChat = document.createElement('input');
-   inputChat.className = 'inputChat';
-   makeDiv.append(inputChat);
-   inputUl = document.createElement('ul');
-   inputUl.className = 'messageArea';
-   chatDiv.append(inputUl);
-   const btnChat = document.createElement('button');
-   btnChat.className = 'btnChat';
-   makeDiv.append(btnChat);
-   btnChat.innerText = "보내기";
-   btnChat.addEventListener('click', sendMessage, true);
-   connect()
+   
    console.log('chat화면 띄우기 완료')
+}
+
+function chatList() {
+   fetch('/chat/rooms').then(function(response) {
+      response.text().then(function(text) {
+         openChat()
+         document.querySelector('#chatContainer').innerHTML = text;
+      })
+   })
+}
+
+
+
+function chatDetail(roomId, member) {
+   console.log(roomId)
+   console.log('왜 들어와짐?')
+	$(function() {
+		var chatBox = $('.chat-box');
+		var messageInput = $('input[name="message"]');
+		message = messageInput;
+		var sendBtn = $('.send');
+      var sock = new SockJS("/ws");
+		var client = Stomp.over(sock); // 1. SockJS를 내부에 들고 있는 client를 내어준다.
+		// 2. connection이 맺어지면 실행된다.
+		client.connect({}, function() {
+			// 3. send(path, header, message)로 메시지를 보낼 수 있다.
+			client.send('/publish/chat/join', {}, JSON.stringify({
+				chatRoomId : roomId,
+				writer : member
+			}));
+			// 4. subscribe(path, callback)로 메시지를 받을 수 있다. callback 첫번째 파라미터의 body로 메시지의 내용이 들어온다.
+			client.subscribe('/subscribe/chat/room/' + roomId, function(
+					chat) {
+            var content = JSON.parse(chat.body);
+            if(content.writer==member){
+               chatBox.append('<li class="myId"><span class="myMember">' + content.message + '</span>('
+						+ content.writer + ')</li>')
+            }else{
+               chatBox.append('<li class="otherId"><span class="otherMember">' + content.message + '</span>('
+						+ content.writer + ')</li>')
+            }
+				
+			});
+      });
+      sendBtn.click(function() {
+         var message = messageInput.val();
+         console.log(message);
+         if(message ==null || message==''){
+            alert('Please Enter Content')
+            
+         }else{
+            client.send('/publish/chat/message', {}, JSON.stringify({
+               chatRoomId : roomId,
+               message : message,
+               writer : member
+            }));
+            messageInput.val('');
+         }
+      });
+	});
+}
+
+function TestDetail(roomId,member){
+   var url = '/chat/rooms/' + roomId
+   console.log('roomId:'+roomId)
+   console.log('member:'+member)
+   console.log('url:'+url)
+   fetch(url).then(function(response) {
+		response.text().then(function(text) {
+			chatDetail(roomId, member)
+			document.querySelector('#chatContainer').innerHTML = text;
+		})
+	})
+}
+
+function saveRoomId(tmp){
+   roomId = tmp;
+}
+
+function chatListDetail(temp, mem) {
+	roomId = temp;
+	member = mem;
+	var url = '/chat/rooms/' + temp
+	console.log(url)
+	fetch(url).then(function(response) {
+		response.text().then(function(text) {
+			chatDetail(roomId, mem)
+			document.querySelector('#chatContainer').innerHTML = text;
+		})
+	})
+}
+function removeChatPage(){
+	let chatContainer = document.querySelector('#chatContainer');
+	chatContainer.remove();
 }
 
 
@@ -68,6 +183,7 @@ function openboard() {
 
 function changeLocation(location) {
 	const boardContainer = document.querySelector('.boardContainer');
+	//fetchBoard(boardContainer, location);
 	fetchBoard(boardContainer, location);
 }
 
@@ -79,87 +195,21 @@ function fetchBoard(ele, location) {
 	})
 }
 
-function goToEditor() {
-   const boardContainer = document.querySelector('.boardContainer');
-	fetch('/boardRegMod').then(function(response) {
-		response.text().then(function(text) {
-			boardContainer.innerHTML = text;
-		}).then(function() {
-         CKEDITOR.replace('board-regMod-ctnt__textarea');
-      })
-	})
-}
-
 function closeContainer(ele) {
     ele.remove();
 }
 
-function onConnected(){
-   console.log("I don't know")
-   stompClient.subscribe('/topic/public', onMessageReceived);
-   stompClient.send("/app/chat.addUser",
-   {},
-   JSON.stringify({sender: username, type: 'JOIN'})
-   )
-   console.log("onConnected 끝")
-}
 
-function onError(error){
-   connectingElement.textContent = "ConnectionError";
-   connectingElement.style.color = 'red';
-}
 
-function sendMessage(event){
-   console.log('message전송 시작')
-   var messageContent = inputChat.value.trim();
-   console.log('messageContent: ' + messageContent)
-   if(messageContent && stompClient){
-      var chatMessage = {
-         sender : username,
-         content: inputChat.value,
-         type : 'CHAT'
-      };
-   stompClient.send("/app/chat.sendMessage",{},JSON.stringify(chatMessage));
-   inputChat.value ='';
-   }
-   event.preventDefault();
-}
-
-function onMessageReceived(payload){
-   var message = JSON.parse(payload.body);
-   
-   var messageElement = document.createElement('li');
-   console.log('message.type:' + message.type)
-   console.log('message.sender: ' + message.sender)
-   if(message.type === "JOIN"){
-      message.content = message.sender + "joined!";
-   }else if(message.type === "LEVAE"){
-      message.content = message.sender + "left!";
-   }else{
-      /* 채팅창에 채팅 메세지 띄우기 */
-      messageElement.className='chat-message';
-      inputUl.append(messageElement);
-      
-      var usernameElement = document.createElement('span');
-      var usernameText = document.createTextNode(message.sender);
-      usernameElement.appendChild(usernameText);
-      messageElement.appendChild(usernameElement);   
-   }
-   var textElement = document.createElement('p');
-   var messageText = document.createTextNode(message.content);
-   textElement.appendChild(messageText);
-   
-   messageElement.appendChild(textElement);
-}
-
-function userMenuInit(isLogin) {
+function userMenuInit(isLogin,temp) {
+   member = temp;
     const userMenu = document.querySelector('.userMenu');
     const alertMenu = document.querySelector('.alertMenu');
     if(alertMenu != null) {
         closeContainer(alertMenu);
     }
    if(userMenu == null) {
-      openUserMenu(isLogin);
+      openUserMenu(isLogin,temp);
    } else {
       closeContainer(userMenu);
    }
@@ -178,51 +228,97 @@ function alertMenuInit() {
    }
 }
 
-function openUserMenu(isLogin) {
+function openUserMenu(isLogin,temp) {
     const makeDiv = document.createElement('div');
     makeDiv.className = 'userMenu';
     makeDiv.style.position = 'absolute';
     makeDiv.style.right = '35px';
-   makeDiv.style.width = '150px';
-   makeDiv.style.height = '350px';
-   makeDiv.style.borderRadius = '5px';
+    makeDiv.style.width = '300px';
+    makeDiv.style.height = '350px';
+    makeDiv.style.borderRadius = '5px';
     makeDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
-   makeDiv.style.color = 'white';
+    makeDiv.style.color = 'white';
     makeDiv.style.zIndex = '2';
 
    const makeDiv2 = document.createElement('div');
    makeDiv2.className = 'userMenuTooltip';
    
+   makeDiv2.style.position = 'relative';
+   makeDiv2.style.top = '50px';
+ 
+   
    const makeSpan2_1_1 = document.createElement('span');
    makeSpan2_1_1.classList.add('cursor');
    makeSpan2_1_1.innerText = '로그인';
    makeSpan2_1_1.setAttribute('onclick',"showLogin()");
-//   const makeSpan2_1_2 = document.createElement('span');
-//   makeSpan2_1_2.classList.add('cursor');
-//   makeSpan2_1_2.innerText = '회원가입';
-//   makeSpan2_1_2.setAttribute('onclick',"location.href='/join'");
    
    const makeSpan2_2_1 = document.createElement('span');
    makeSpan2_2_1.classList.add('cursor');
    makeSpan2_2_1.innerText = '로그아웃';
    makeSpan2_2_1.setAttribute('onclick',"location.href='/logout'");
-   
+   makeSpan2_2_1.style.position = 'relative';
+   makeSpan2_2_1.style.top = '170px';
+   makeSpan2_2_1.style.left = '0px';
+   makeSpan2_2_1.style.display = 'flex';
+   makeSpan2_2_1.style.justifyContent = 'center';
+   console.log(temp)
+   console.log(isLogin)
+	
    const makeSpan2_2_2 = document.createElement('span');
    makeSpan2_2_2.classList.add('cursor');
    makeSpan2_2_2.innerText = '마이페이지';
-   makeSpan2_2_2.setAttribute('onclick', 'openUserInfo()');
+   makeSpan2_2_2.setAttribute('onclick', 'openUserInfo(0)');
+   makeSpan2_2_2.style.position = 'relative';
+   makeSpan2_2_2.style.top = '100px';
+   makeSpan2_2_2.style.left = '0px';
+   makeSpan2_2_2.style.display = 'flex';
+   makeSpan2_2_2.style.justifyContent = 'center';
    
-//   const makeSpan2_2_2 = document.createElement('span');
-//   makeSpan2_2_2.classList.add('cursor');
-//   makeSpan2_2_2.innerText = '메인화면';
-//   makeSpan2_2_2.setAttribute('onclick',"location.href='/'");
-   
+   const makeSpan2_2_3 = document.createElement('span');
+   makeSpan2_2_3.classList.add('cursor');
+   makeSpan2_2_3.innerText = temp;
+   makeSpan2_2_3.setAttribute('onclick', 'openUserInfo(0)');
+   makeSpan2_2_3.style.position = 'relative';
+   makeSpan2_2_3.style.top = '90px';
+   makeSpan2_2_3.style.left = '0px';
+   makeSpan2_2_3.style.display = 'flex';
+   makeSpan2_2_3.style.justifyContent = 'center';
+
+   const makeSpan2_2_4 = document.createElement('span');
+   makeSpan2_2_4.classList.add('material-icons');
+   makeSpan2_2_4.innerText = 'person_add';
+   makeSpan2_2_4.style.position = 'relative';
+   makeSpan2_2_4.style.top = '140px';
+   makeSpan2_2_4.style.left = '0px';
+   makeSpan2_2_4.style.display = 'flex';
+   makeSpan2_2_4.style.justifyContent = 'center';
+   makeSpan2_2_4.style.alignItems = 'center';
+   makeSpan2_2_4.style.fontSize = '1.8em';
+
+  
+
+   const makeSpan2_2_5 = document.createElement('span');
+   makeSpan2_2_5.classList.add('cursor');
+   makeSpan2_2_5.innerText = '계정 추가';
+   makeSpan2_2_5.style.fontSize = '18px';
+   makeSpan2_2_4.addEventListener('click', ()=>{
+	openUserInfo(3);
+	});//userInfo3으로 바로가기로 바꿔야함
+
+   //makeSpan2_2_1.style.display = ' block';
+   //makeSpan2_2_2.style.display = ' block';
+   //makeSpan2_2_4.style.display = ' block';
+
+
    if(isLogin) {
-      makeDiv2.append(makeSpan2_2_1);
+      makeDiv2.append(makeSpan2_2_3);
       makeDiv2.append(makeSpan2_2_2);
+      makeSpan2_2_4.append(makeSpan2_2_5);
+      makeDiv2.append(makeSpan2_2_4);
+      makeDiv2.append(makeSpan2_2_1);
+	  
    } else {
       makeDiv2.append(makeSpan2_1_1);
-//      makeDiv2.append(makeSpan2_1_2);
    }
    makeDiv.append(makeDiv2);
    centralMenu1_2.append(makeDiv);
@@ -252,8 +348,9 @@ function makeLogin() {
    let loginWindowContainer = document.createElement('div');
    loginWindowContainer.classList.add('loginWindowContainer');
    loginWindowContainer.setAttribute('id', 'loginWindowContainer');
-   loginWindowContainer.addEventListener('click', event);
+	   loginWindowContainer.addEventListener('click', event);
    
+
    let loginPageContainer = document.createElement('div');
    loginPageContainer.classList.add('loginPageContainer');
    
@@ -278,6 +375,7 @@ function makeLogin() {
    body.prepend(loginWindowContainer);
 }
 
+
 function showLogin() {
    fetch('/login').then(function(response) {
       response.text().then(function(text) {
@@ -292,12 +390,27 @@ function removeLogin() {
    loginWindowContainer.remove();
 }
 
+function moveToJoin(){
+	removeLogin();
+	makeJoin();
+}
+
+function moveToLogin(){
+	removeLogin();
+	showLogin();
+}
+
+function makeJoin(){
+   fetch('/join').then(function(response) {
+      response.text().then(function(text) {
+		   makeLogin();
+		   document.querySelector('#loginWindow').innerHTML = text;
+      })
+   })
+}
 
 
-
-/*usernameForm.addEventListener('submit', connect, true)*/
-
-function openUserInfo() {
+function openUserInfo(idx) {
    const body = document.querySelector('body');
    const makeDiv = document.createElement('div');
    makeDiv.classList.add('shadowWindow');
@@ -305,7 +418,11 @@ function openUserInfo() {
    
    const makeDiv2 = document.createElement('div');
    makeDiv2.classList.add('loginPageContainer');
-   
+
+
+   const myPageTabMenuContainer = document.createElement('div');
+   myPageTabMenuContainer.classList.add('myPageTabMenuContainer');
+
    const makeDiv2_1 = document.createElement('div');
    makeDiv2_1.classList.add('closeLoginWindow');
    
@@ -322,12 +439,27 @@ function openUserInfo() {
    fetch('/userinfo').then(function(response) {
       response.text().then(function(text){
          makeDiv2_2.innerHTML = text;
-      })
+      }).then(function () {openMyPageDetails(idx)});
    })
    
+	function openMyPageDetails(idx) { 
+		const tabBoxContainer = document.querySelector('.tabBoxContainer'); 
+		if(idx == 4) {
+			location.href = 'http://localhost:8080/logout'; 
+			return; 
+		} // jsp 파일 이름이 바뀌면 controller와 pageName이 변경되어야 한다. 
+		const pageName = '/userinfo' + idx; 
+		fetch(pageName).then(function(response) { 
+			response.text().then(function(text) { 
+				tabBoxContainer.innerHTML = text; 
+			}) 
+		})
+	}
+	
    makeDiv2_1.append(makeSpan2_1_1);
-   makeDiv2.append(makeDiv2_1);
-   makeDiv2.append(makeDiv2_2);
+   myPageTabMenuContainer.append(makeDiv2_1);
+   myPageTabMenuContainer.append(makeDiv2_2);
+   makeDiv2.append(myPageTabMenuContainer);
    makeDiv.append(makeDiv2);
    body.prepend(makeDiv);
 }
--- bin/src/main/webapp/WEB-INF/jsp/index.jsp
+++ bin/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -4,38 +4,46 @@
 <!DOCTYPE html>
 <sec:authorize var="isLogin" access="isAuthenticated()" />
 <html>
-<head>
+
+<head>  
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${title}</title>
-    <link rel="icon" href="/img/favicon.png">
-    <link rel="stylesheet" href="/css/index.css?ver=8">
-    <link rel="stylesheet" href="/css/boardList.css?ver=3">
-    <link rel="stylesheet" href="/css/boardDetail.css?ver=4">
-    <link rel="stylesheet" href="/css/boardRegMod.css?ver=1">
-    <link rel="stylesheet" href="/css/login.css?ver=2">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-     <!-- 아웃라인 material-icon 링크 추가 -->
-    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
-	<!-- 위지윅 에디터 추가 -->
-	<script src="//cdn.ckeditor.com/4.15.0/standard/ckeditor.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${title}</title>
+<link rel="icon" href="/img/favicon.png">
+<link rel="stylesheet" href="/css/index.css?ver=9544">
+<link rel="stylesheet" href="/css/boardList.css?ver=3">
+<link rel="stylesheet" href="/css/boardDetail.css?ver=4">
+<link rel="stylesheet" href="/css/boardRegMod.css?ver=1">
+<link rel="stylesheet" href="/css/comment.css?ver=8">
+<link rel="stylesheet" href="/css/login.css?ver=27">
+<link rel="stylesheet" href="/css/join.css?ver=89">
+<link rel="stylesheet" href="/css/test.css?ver=5">
+
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+
+<!-- 아웃라인 material-icon 링크 추가 -->
+<link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+<script src="//cdn.ckeditor.com/4.15.0/standard/ckeditor.js"></script>
 </head>
+
 <body>
 	<div id="bg1">
 		<img src="/img/yousef-salhamoud-kQ6mh2yagDw-unsplash.jpg" alt="" id="bg1_1">
 	</div>
 	<main class="centralContainer">
 		<header class="centralHeader">
-			<div class="centralLogo">
+			<div class="centralLogo cursor" onclick="location.href='/main'">
 				<img id="centralLogoImage" src="/img/logowhite.png" alt="">
 			</div>
 			<div class="centralSearch">
-				<form action="" method="POST" class="centralSearchFrm">
+				<form action="" method="POST" class="centralSearchFrm" onsubmit="findVideo(event)">
 					<div class="centralSearch1_1">
-						<input type="text" name="" id="" class="Search1_1__input" placeholder="검색">
+						<input type="text" name="searchVideo" id="searchVideo" class="Search1_1__input" placeholder="검색">
 					</div>
 					<div class="centralSearch1_2">
-						<span class="material-icons headMenus" onclick="submit()">search</span>
+						<span class="material-icons headMenus" onclick="findVideo(event)">search</span>
 					</div>
 				</form>
 			</div>
@@ -44,157 +52,81 @@
 					<span class="material-icons" onclick="alertMenuInit()">notifications_none</span>
 				</div>
 				<div class="menus centralMenu1_2">
-					<span class="material-icons" onclick="userMenuInit(${isLogin})">person_outline</span>
+					<span class="material-icons" onclick="userMenuInit(${isLogin},'${userInfo.nickname }')">person_outline</span>
 				</div>
 			</div>
 		</header>
 		<section class="centralSection">
-			<!-- jsp include -->
 			<div class="sectionContainer">
-				<div class="slideContainer">
-					<%-- <video autoplay="autoplay" controls>
-                		<source src = "/videos/${videoName }" type="video/mp4">	
-                	</video> --%>
-
-
-					<div class="slide slide__left">
-						<span class="material-icons">keyboard_arrow_left</span>
-					</div>
-					<div class="slide slide__center" id="twitch-embed">
-						<sec:authorize access="isAuthenticated()">
-							<div id="video"></div>
-						</sec:authorize>
-					</div>
-					<div class="slide slide__right">
-						<span class="material-icons">keyboard_arrow_right</span>
-					</div>
-				</div>
-				<!-- 채팅관련 창 나오는 곳  -->
+				<div class="slideContainer"></div>
 			</div>
 			<aside class="centralSidebar">
 				<div class="menus sidebarMenu1_1">
 					<span class="material-icons" onclick="boardInit()">assignment</span>
 				</div>
 				<div class="menus sidebarMenu1_2">
-					<span class="material-icons" onclick="chatInit()">chat</span>
+					<span class="material-icons" onclick="chatInit('${member}')">chat</span>
 				</div>
 			</aside>
 		</section>
 	</main>
+
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.4.0/sockjs.min.js"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js"></script>
+	<script src="https://embed.twitch.tv/embed/v1.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.4.0/sockjs.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src='https://unpkg.com/react-player/dist/ReactPlayer.standalone.js'></script>
+	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
-    <script src="/js/index.js?aaa=11"></script>
-    <script src="/js/login.js"></script>
-    <script src="/js/boardList.js?ver=4"></script>
-    <script src="/js/boardDetail.js?ver=1"></script>
-    <script src="/js/boardRegMod.js?ver=1"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <!-- 트위치 채널 긁어오기(채널지정) -->
-    <script src="https://embed.twitch.tv/embed/v1.js"></script>
-    
-    <script>
-    function connect(event){
-    	username = '${userInfo.username}';
-    	console.log(username)
-    	if(username){
-    		console.log("connect Start")
-    		var socket = new SockJS('/ws');
-    		stompClient = Stomp.over(socket);
-    		
-    		stompClient.connect({},onConnected, onError);
-    	}
-    	/*event.preventDefault();*/
-    }
-    /* $.ajax({
-    	 type: 'GET',
-    	 url: 'https://api.twitch.tv/kraken/channels/twitch',
-    	 headers: {
-    	   'Client-ID': '95put5vj4zw23iiy5poetela3ilbq6'
-    	 },
-    	 success: function(data) {
-    	   console.log(data);
-    	 }
-    	});
-      
-    xhr.send(); */
-    /* new Twitch.Embed("twitch-embed", {
-        width: 854,
-        height: 480,
-        channel: "2chamcham2",
-        // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-        parent: ["embed.example.com", "othersite.example.com"]
-      }); */
-	</script>
+	<script src="/js/index.js?aaa=75092"></script>
+	<script src="/js/search.js?aa=afasef"></script>
+	<script src="/js/test.js?ver=13"></script>
+	<script src="/js/boardRegMod.js?ver=45"></script>
+	<script src="/js/boardList.js?ver=80"></script>
+	<script src="/js/boardDetail.js?ver=1"></script>
+	<script src="/js/join.js?ver=23"></script>
+	<script src="/js/comment.js?ver=3"></script>
+	<!-- 트위치 채널 긁어오기(채널지정) -->
 
 	<script>
-  const underline = document.getElementsByClassName('tabMenuBtn')
-
-  function temp(idx) {
-    openMyPageDetails(idx)
-    addUnderLine(idx)
-    //removeUnderLine(idx)
-  }
-
-  function addUnderLine(idx) {
-    if(document.querySelector('.underline')) {
-      const under_element = document.querySelector('.underline')
-        under_element.classList.remove('underline')
-    }
-    underline[idx].classList.add('underline')
-  }
-
-  function openMyPageDetails(idx) {
-  const tabBoxContainer = document.querySelector('.tabBoxContainer');
-  
-	if(idx == 4) {
-	  location.href = 'http://localhost:8080/logout';
-	  return;
-	}
-
-	// jsp 파일 이름이 바뀌면 controller와 pageName이 변경되어야 한다.
-	const pageName = '/userinfo' + idx;
-	fetch(pageName).then(function(response) {
-		response.text().then(function(text) {
-			tabBoxContainer.innerHTML = text;
-		})
-	})
-  }
-
-</script>
+	
+	
+	/* Profile  Eunjeong Start */
+	
+	/* Eunjeong End */
+	</script>
 
 	<sec:authorize access="isAuthenticated()">
-		<script src='https://unpkg.com/react-player/dist/ReactPlayer.standalone.js'></script>
-		<c:if test="${userInfo.sns == 'twitch'}">
-			<script type="text/javascript">
+	
+		<c:if test="${userInfo != null}">
+
+			<script>
+				//console.log('userInfo= ${userInfo}')
+			</script>
+		</c:if>
 		
-			function getVideo(res){
-				axios.get('/CallVideo',{
-					params:{
-						follow : res.data.data[0].to_id
-						}
-				}).then(function(res){
-					console.log(res)
-					const url = `\${res.data.data[0].url}`;
-					 const container = document.getElementById('video')
-					 renderReactPlayer(container , {
-						 url,
-						 playing: true,
-						 controls: true
-					 })
-				})	
-			}
-			
-			axios.get('/CallFollows',{}).then(function(res){
-				console.log(res)
-				getVideo(res)
-			})
-			axios.get('/getStreams',{}).then(function(res){
-				console.log(res)
-			})
-			
-	    </script>
+		<c:if test="${userDomain != null}">
+			<script>
+				//console.log('userDomain = ${userDomain}')
+			</script>
+			<c:forEach items="${userDomain.sns}" var="item">
+				<c:if test="${item == 'twitch'}">
+				<script>
+				/* member = '${member}'; */	
+				</script>
+					<script type="text/javascript" src="/js/twitch.js">
+					</script>
+				</c:if>
+			</c:forEach>
 		</c:if>
 	</sec:authorize>
 </body>
+
 </html>
