737a918fe908e27667aca05515e9375a21d532f9
류욱준 <corn1107@hanmail.net>
2020-11-10 10:02:11 +0900

    ...?

--- src/main/java/com/amolrang/modume/repository/ChatRoomRepository.java
+++ src/main/java/com/amolrang/modume/repository/ChatRoomRepository.java
@@ -24,48 +24,54 @@ public class ChatRoomRepository {
 	private Map<String, ChatRoom> chatRoomMap;
 	@Getter
 	private Collection<ChatRoom> chatRooms;
-	public String CreateRoom(String roomName,HttpSession hs) {
+
+	public String CreateRoom(String roomName, HttpSession hs) {
 //		ChatRoom[] roomList = {ChatRoom.create("전체 채팅방"),ChatRoom.create("?????")};
-		ArrayList<Map> list = (ArrayList<Map>)hs.getAttribute("LiveStream");
-		String JsonId = (String)hs.getAttribute("streamerID");
+		ArrayList<Map> list = (ArrayList<Map>) hs.getAttribute("LiveStream");
+		String JsonId = (String) hs.getAttribute("streamerID");
 		ChatRoom roomList = null;
 		String streamerID = null;
-		String check = "false";
-		if(list != null) {
+		Boolean check = false;
+		log.info("test:{}",(Boolean)hs.getAttribute("check").equals(true));
+		if (list != null) {
+			log.info("test:{}","Twitch Check");
 			System.out.println(list.size());
-			for(int i=0; i<list.size(); i++) {
-				//방송하는 사람의 이름 + 닉네임
-				String userName = (String)list.get(i).get("user_name");
-				String url = (String)list.get(i).get("thumbnail_url");
+			for (int i = 0; i < list.size(); i++) {
+				// 방송하는 사람의 이름 + 닉네임
+				String userName = (String) list.get(i).get("user_name");
+				String url = (String) list.get(i).get("thumbnail_url");
 				streamerID = url.substring(url.indexOf("user_") + 5, url.lastIndexOf("-{width}"));
-				if(streamerID.equals(JsonId)) {
-					roomList= ChatRoom.create(JsonId,userName);
+				if (streamerID.equals(JsonId)) {
+					roomList = ChatRoom.create(JsonId, userName);
 				}
 			}
-		} /*
-			 * else if((boolean)hs.getAttribute("check").equals(check)){ JsonId =
-			 * (String)hs.getAttribute("youTubeRoomId"); roomList =
-			 * ChatRoom.create(JsonId,(String)hs.getAttribute("youTubeTitle")); }
-			 */
+			check = true;
+			hs.setAttribute("check", check);
+		} else if ((Boolean)hs.getAttribute("check").equals(true)) {
+			log.info("test:{}","Youtube Check");
+			JsonId = (String) hs.getAttribute("youTubeRoomId");
+			roomList = ChatRoom.create(JsonId, (String) hs.getAttribute("youTubeTitle"));
+			log.info("JsonId:{}",JsonId);
+			log.info("roomList:{}",roomList);
+		}
 		else {
 			roomList = ChatRoom.create("All", "전체채팅방");
 		}
-		log.info("roomList:{}",roomList);
-		if( roomList != null ) {
-			chatRoomMap = Collections
-					.unmodifiableMap(Stream.of(roomList)
-							.collect(Collectors.toMap(ChatRoom::getName, Function.identity())));
-			//System.out.println(chatRoomMap);
+		log.info("roomList:{}", roomList);
+		if (roomList != null) {
+			chatRoomMap = Collections.unmodifiableMap(
+					Stream.of(roomList).collect(Collectors.toMap(ChatRoom::getName, Function.identity())));
+			// System.out.println(chatRoomMap);
 			chatRooms = Collections.unmodifiableCollection(chatRoomMap.values());
-			log.info("chatRooms:{}",chatRooms);
+			log.info("chatRooms:{}", chatRooms);
 		}
-		log.info("chatRoomMap:{}",chatRoomMap);
+		log.info("chatRoomMap:{}", chatRoomMap);
 		hs.setAttribute("room", roomList);
 		return JsonId;
 	}
-	
+
 	public ChatRoom getChatRoom(String name) {
-		log.info("getChatRoom_name:{}",name);
+		log.info("getChatRoom_name:{}", name);
 		return chatRoomMap.get(name);
 	}
 
