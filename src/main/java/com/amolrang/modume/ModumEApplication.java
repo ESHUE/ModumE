05b92c4ab20ad687a5802fabc1f8d0ab920919bd
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-11-18 00:04:30 +0900

    http -> https 변경해주는 톰캣 설정 추가

--- src/main/java/com/amolrang/modume/ModumEApplication.java
+++ src/main/java/com/amolrang/modume/ModumEApplication.java
@@ -2,10 +2,17 @@ package com.amolrang.modume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ModumEApplication {
+public class ModumEApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ModumEApplication.class, args);
 	}
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(ModumEApplication.class);
+	  }
 }
